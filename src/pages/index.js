2ec28ffe9c47dbc292e7a046a47c38ab659ee6a7
nancy <liyingxiong23@gmail.com>
2022-05-24 23:01:46 +1000

    Finished Gatsby Tutorial Part3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        // src="https://img.freepik.com/free-vector/cute-bunny-holding-carrot-cartoon-illustration_403370-193.jpg?w=2000"
+        src="../images/rabbit.jpeg"
+        alt="a little rabbit"
+      />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
